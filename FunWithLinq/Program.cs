0b94098c9c9c06607bade98d2d0e6bdf8e6886e9
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-10-08 21:24:03 +0200

    LINQ

---
+++ FunWithLinq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>() { 1, 5, 7, 1, 13, 4, 346, 23, 7432, 2, 34, 14, 86, 25, 53, 83, 47, 2657, 264647, 375757, 36261, 1233 };
+
+            Console.WriteLine("Unsorted.");
+            foreach (int num in numList)
+            {
+                Console.Write(num + " ");
+            }
+
+            var sortEvenNum = numList.Where(x => x % 2 == 0).OrderBy(y => y).ToList();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted.");
+            foreach (int num in sortEvenNum)
+            {
+                Console.Write(num + " ");
+            }
+
+            var sortOddNum = numList.Where(x => x % 2 == 1).OrderBy(y => y).ToList();
+            foreach (int num in sortOddNum)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
